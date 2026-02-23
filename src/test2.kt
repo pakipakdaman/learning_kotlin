0b798433322b7499086a7a6cf8deed5593029f88
pakipakdaman <parsa1395pakdaman@gmail.com>
2026-02-23 12:26:44 +0330

    Initial commit

---
+++ src/test2.kt
@@ -0,0 +1,20 @@
+/*fun main(){
+    val num1 : Int = 15
+    val num2 : Int = 4
+    val pi : Double = 3.14159
+    val num3 : Double = 2.1
+    println("${num1+num2}")
+    println("${num1-num2}")
+    println("${num1* num2}")
+    println("${num1/num2}")
+    println("${pi/num3}")
+    println("${pi%num3}")
+    println("${pi*2}")
+
+
+
+
+}*/
+fun main(){
+
+}
